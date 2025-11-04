727368305b76b8d400fc65b584cb1633229e4395
Mirco Meiners <Mirco.Meiners@hs-bremen.de>
2025-11-04 12:19:08 +0100

    SCPI scripts

---
+++ files/meas/data/diode/rsScopeData_elie3/1N4001/concat_csv.py
@@ -0,0 +1,15 @@
+# import os
+import glob
+import pandas as pd
+
+# os.chdir("./")
+
+extension = 'CSV'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+
+# combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+
+# export to csv
+combined_csv.to_csv("rs_elie3_1N4001.csv", index=False, encoding='utf-8-sig')
